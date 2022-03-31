9738bfde105380ae7e3fdd47574356cfc12a775f
Jordan Foote <82241562+jordanfoote@users.noreply.github.com>
2022-03-31 09:16:34 -0500

    Updating

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const baseURL = 'https://api.themoviedb.org/3/movie/550?api_key=6c44463bf264a541103d4abb9dea5861'
+
+fetch(baseURL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    });
+
+    let count = 0;
+    let max = 2;
+    let interval = 1000;
+    const loadingPage = document.getElementById('loading-page');
+
+    var intervalId = setInterval(function () {
+        if (count >= max) {
+            loadingPage.innerHTML = 'Movie App will display here';
+        } else {
+            count++;
+        }
+    }, interval);
